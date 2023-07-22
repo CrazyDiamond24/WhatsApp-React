81413efb5f30572ad11394bd6c9434481edc255a
Itay komemi <itaykomemi8@gmail.com>
2023-07-22 19:44:52 +0300

    merge

--- src/cmps/UserList.jsx
+++ src/cmps/UserList.jsx
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-import { UserPreview } from './UserPreview'
-export function UserList({ users, onRemoveUser, onSelectContact }) {  
-=======
 import { useEffect, useState } from "react"
 import { UserPreview } from "./UserPreview"
 import { useSelector } from "react-redux"
@@ -24,7 +20,6 @@ export function UserList({ filterBy, onRemoveUser, onSelectContact }) {
       )
     })
   }
->>>>>>> cda4b3f548106ad1ee6fd0ad2e3a586c6a673201
   return (
     <section className="user-list simple-cards-grid">
       {filteredUsers?.map((user) => (
