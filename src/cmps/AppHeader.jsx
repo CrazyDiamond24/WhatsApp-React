9f83d6efe066c7ea366d6647387e268231612a02
Itay komemi <itaykomemi8@gmail.com>
2023-08-06 17:18:11 +0300

    push pull

--- src/cmps/AppHeader.jsx
+++ src/cmps/AppHeader.jsx
@@ -1,36 +1,36 @@
-import { useDispatch, useSelector } from 'react-redux'
-import { Link, useNavigate } from 'react-router-dom'
-import LogoWithoutWord from '../assets/imgs/Logo-without-word.png'
-import { getSpotifySvg } from '../services/SVG.service'
-import { useState } from 'react'
-import { UserPref } from './UserPref'
-import { doLogout , editUserProfile } from '../store/actions/user.actions'
+import { useDispatch, useSelector } from "react-redux"
+import { Link, useNavigate } from "react-router-dom"
+import LogoWithoutWord from "../assets/imgs/Logo-without-word.png"
+import { getSpotifySvg } from "../services/SVG.service"
+import { useState } from "react"
+import { UserPref } from "./UserPref"
+import { doLogout, editUserProfile } from "../store/actions/user.actions"
 
 export function AppHeader({ showProfile }) {
   const [showModal, setShowModal] = useState(false)
   const user = useSelector((storeState) => storeState.userModule.loggedInUser)
-  const [editedUser, setEditedUser] = useState({...user})
+  const [editedUser, setEditedUser] = useState({ ...user })
   const dispatch = useDispatch()
   const navigate = useNavigate()
 
   function showPrefsModal() {
     setShowModal(!showModal)
   }
-  
+
   function handelLogout() {
     editedUser.lastSeen = Date.now()
-    dispatch((editUserProfile(editedUser)))
+    dispatch(editUserProfile(editedUser))
     dispatch(doLogout())
-    navigate('/')
+    navigate("/")
   }
 
   return (
     <header className="app-header">
       <button onClick={handelLogout}>Logout</button>
-      {/* <span
+      <span
         onClick={showPrefsModal}
         dangerouslySetInnerHTML={{
-          __html: getSpotifySvg('plusWhatsapp'),
+          __html: getSpotifySvg("plusWhatsapp"),
         }}
       ></span>
       <section className="user-header">
