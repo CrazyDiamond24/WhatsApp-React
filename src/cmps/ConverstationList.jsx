30b5aa6efb9d27b81c33e01a9421c931ec015463
rania <raniakittan@hotmail.com>
2023-08-08 10:35:59 +0300

    daily check

--- src/cmps/ConverstationList.jsx
+++ src/cmps/ConverstationList.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import { ImgMsg } from './MsgTypies/ImgMsg'
 import { VideoMsg } from './MsgTypies/VideoMsg'
 import { AudioMsg } from './MsgTypies/AudioMsg'
@@ -8,14 +8,27 @@ import { MsgOptions } from './MsgOptions'
 import { msgService } from '../services/msg.service'
 
 export function ConverstationList({ msgs, loggedInUser, isHovered, user }) {
+  const chatMsgRefs = useRef([])
+
   function showTimestamp(timestamp) {
     return msgService.getTimestamp(timestamp)
   }
+
+  useEffect(() => {
+    chatMsgRefs.current.forEach((msgRef, index) => {
+      if (msgRef && msgRef.offsetWidth > 200) {
+        msgRef.classList.add('wide-msg')
+      } else {
+        msgRef.classList.remove('wide-msg')
+      }
+    })
+  }, [msgs])
 
   return (
     <>
       {msgs?.map((msg, index) => (
         <li
+          ref={(el) => (chatMsgRefs.current[index] = el)}
           key={index}
           className={`chat-msg ${
             msg.senderId === loggedInUser?._id ? 'sent' : 'received'
@@ -27,7 +40,7 @@ export function ConverstationList({ msgs, loggedInUser, isHovered, user }) {
           {msg.type === 'text' && <TextMsg msg={msg} />}
           {msg.type === 'file' && <FileMsg msg={msg} />}
 
-          <span className="timestamp">{showTimestamp(msg.timestamp)}</span>
+          <span className='timestamp'>{showTimestamp(msg.timestamp)}</span>
           {isHovered === index && (
             <MsgOptions user={user} msg={msg} loggedInUser={loggedInUser} />
           )}
