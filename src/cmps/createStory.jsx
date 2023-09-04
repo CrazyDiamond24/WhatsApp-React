f4f5ffba48292bfff79ae29f0de157b042f49cd4
Itay komemi <itaykomemi8@gmail.com>
2023-09-04 18:41:17 +0300

    create story

--- src/cmps/createStory.jsx
+++ src/cmps/createStory.jsx
@@ -1,18 +1,18 @@
-import React, { useRef, useState, useEffect } from 'react'
-import { uploadImg } from '../services/upload-img.service'
-import { CanvasColorPicker } from './CanvasColorPicker'
-import { useDispatch } from 'react-redux'
-import { addStoryToUser } from '../store/actions/user.actions'
-import { FontFamily } from './FontFamily'
-import { ColorPick } from './svgs/ColorPick'
-import placeholderImg from '../assets/imgs/story-placeholder.png'
-import { StoryLoader } from '../cmps/StoryLoader'
+import React, { useRef, useState, useEffect } from "react"
+import { uploadImg } from "../services/upload-img.service"
+import { CanvasColorPicker } from "./CanvasColorPicker"
+import { useDispatch } from "react-redux"
+import { addStoryToUser } from "../store/actions/user.actions"
+import { FontFamily } from "./FontFamily"
+import { ColorPick } from "./svgs/ColorPick"
+import placeholderImg from "../assets/imgs/story-placeholder.png"
+import { StoryLoader } from "../cmps/StoryLoader"
 
 export function CreateStory(props) {
-  const [text, setText] = useState('')
-  const [textWidth, setTextWidth] = useState('20')
-  const [textColor, setTextColor] = useState('black')
-  const [textFontFamily, setTextFontFamily] = useState('Arial')
+  const [text, setText] = useState("")
+  const textWidth = "20"
+  const textColor = "black"
+  const textFontFamily = "Arial"
   const [currentSentenceIdx, setCurrentSentenceIdx] = useState(0)
   const [imageUrl, setImageUrl] = useState(null)
   const [showColorModal, setShowColorModal] = useState(false)
@@ -20,11 +20,11 @@ export function CreateStory(props) {
   const [isLoading, setIsLoading] = useState(false)
   const [sentences, setSentences] = useState([
     {
-      text: '',
-      color: 'black',
+      text: "",
+      color: "black",
       pos: { x: 50, y: 50 },
-      width: '20',
-      fontFamily: 'Arial',
+      width: "20",
+      fontFamily: "Arial",
     },
   ])
 
@@ -35,12 +35,12 @@ export function CreateStory(props) {
 
   useEffect(() => {
     const canvas = canvasRef.current
-    const ctx = canvas.getContext('2d')
+    const ctx = canvas.getContext("2d")
 
     ctx.clearRect(0, 0, canvas.width, canvas.height)
 
     let img = new Image()
-    img.crossOrigin = 'Anonymous'
+    img.crossOrigin = "Anonymous"
     img.onload = () => {
       const canvasAspectRatio = canvas.width / canvas.height
       const imageAspectRatio = img.width / img.height
@@ -73,16 +73,16 @@ export function CreateStory(props) {
       ctx.fillText(text, textPos.x, textPos.y)
     }
   }, [imageUrl, text, textPos, textColor, textWidth, textFontFamily])
-  
+
   useEffect(() => {
     const canvas = canvasRef.current
-    const ctx = canvas.getContext('2d')
+    const ctx = canvas.getContext("2d")
 
     ctx.clearRect(0, 0, canvas.width, canvas.height)
 
     if (imageUrl) {
       let img = new Image()
-      img.crossOrigin = 'Anonymous'
+      img.crossOrigin = "Anonymous"
 
       img.onload = () => {
         ctx.drawImage(img, 0, 0, canvas.width, canvas.height)
@@ -104,6 +104,26 @@ export function CreateStory(props) {
     }
   }, [imageUrl, sentences])
 
+  useEffect(() => {
+    function handleClickOutside(event) {
+      if (
+        colorModalRef.current &&
+        !colorModalRef.current.contains(event.target)
+      ) {
+        setShowColorModal(false)
+      }
+    }
+
+    document.addEventListener("mousedown", handleClickOutside)
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside)
+    }
+  }, [])
+  function handleClose() {
+    props.onClose()
+  }
+
   function handleMouseDown(e) {
     const rect = canvasRef.current.getBoundingClientRect()
     const x = e.clientX - rect.left
@@ -112,7 +132,7 @@ export function CreateStory(props) {
     for (let i = 0; i < sentences.length; i++) {
       const sentence = sentences[i]
       const textWidth = canvasRef.current
-        .getContext('2d')
+        .getContext("2d")
         .measureText(sentence.text).width
       if (
         y >= sentence.pos.y - parseInt(sentence.width, 10) &&
@@ -144,12 +164,12 @@ export function CreateStory(props) {
   async function handleImageUpload(ev) {
     setIsLoading(true)
     const file =
-      ev.type === 'change' ? ev.target.files[0] : ev.dataTransfer.files[0]
+      ev.type === "change" ? ev.target.files[0] : ev.dataTransfer.files[0]
     try {
       const { url } = await uploadImg(file)
       setImageUrl(url)
     } catch (err) {
-      console.log('err', err)
+      console.log("err", err)
     }
     setIsLoading(false)
   }
@@ -162,23 +182,11 @@ export function CreateStory(props) {
     setShowColorModal(!showColorModal)
   }
 
-  function handleColorSelect(color) {
-    setTextColor(color)
-  }
-
-  function handleWidthSelect(width) {
-    setTextWidth(width)
-  }
-
-  function handleFontFamilySelect(font) {
-    setTextFontFamily(font)
-  }
-
   function addToStory() {
     if (!imageUrl && !text) {
       return
     }
-    const canvasUrl = canvasRef.current.toDataURL('image/png')
+    const canvasUrl = canvasRef.current.toDataURL("image/png")
 
     dispatch(addStoryToUser(canvasUrl))
 
@@ -198,15 +206,15 @@ export function CreateStory(props) {
       setSentences([
         ...sentences,
         {
-          text: '',
-          color: 'black',
+          text: "",
+          color: "black",
           pos: { x: 50, y: 50 + sentences.length * 50 },
-          width: '20',
-          fontFamily: 'Arial',
+          width: "20",
+          fontFamily: "Arial",
         },
       ])
       setCurrentSentenceIdx(sentences.length)
-      setText('')
+      setText("")
     }
   }
 
@@ -216,84 +224,75 @@ export function CreateStory(props) {
     setSentences(updatedSentences)
   }
 
-  function handleColorSelect(color) {
+  // function handleColorSelect(color) {
+  //   const updatedSentences = [...sentences]
+  //   updatedSentences[currentSentenceIdx].color = color
+  //   setSentences(updatedSentences)
+  // }
+
+  // function handleWidthSelect(width) {
+  //   const updatedSentences = [...sentences]
+  //   updatedSentences[currentSentenceIdx].width = width
+  //   setSentences(updatedSentences)
+  // }
+
+  // function handleFontFamilySelect(font) {
+  //   const updatedSentences = [...sentences]
+  //   updatedSentences[currentSentenceIdx].fontFamily = font
+  //   setSentences(updatedSentences)
+  // }
+
+  function handlePropertySelect(property, value) {
     const updatedSentences = [...sentences]
-    updatedSentences[currentSentenceIdx].color = color
+    updatedSentences[currentSentenceIdx][property] = value
     setSentences(updatedSentences)
-  }
-
-  function handleWidthSelect(width) {
-    const updatedSentences = [...sentences]
-    updatedSentences[currentSentenceIdx].width = width
-    setSentences(updatedSentences)
-  }
-
-  function handleFontFamilySelect(font) {
-    const updatedSentences = [...sentences]
-    updatedSentences[currentSentenceIdx].fontFamily = font
-    setSentences(updatedSentences)
-  }
-
-  useEffect(() => {
-    function handleClickOutside(event) {
-      if (
-        colorModalRef.current &&
-        !colorModalRef.current.contains(event.target)
-      ) {
-        setShowColorModal(false)
-      }
-    }
-
-    document.addEventListener('mousedown', handleClickOutside)
-
-    return () => {
-      document.removeEventListener('mousedown', handleClickOutside)
-    }
-  }, [])
-  function handleClose() {
-    props.onClose()
   }
 
   return (
     <>
-      <div className='overlay'></div>
+      <div className="overlay"></div>
 
-      <div className='create-story'>
+      <div className="create-story">
         {isLoading && <StoryLoader />}
-        <button title='Close' className='close-button' onClick={handleClose}>
+        <button title="Close" className="close-button" onClick={handleClose}>
           X
         </button>
         <input
-          placeholder='choose file'
-          type='file'
-          title='Upload image'
+          placeholder="choose file"
+          type="file"
+          title="Upload image"
           onChange={handleImageUpload}
-          className={imageUrl ? 'hidden' : 'story-file-upload'}
+          className={imageUrl ? "hidden" : "story-file-upload"}
         />
 
         <input
-          placeholder='Add text'
-          type='text'
-          value={sentences[currentSentenceIdx]?.text || ''}
+          placeholder="Add text"
+          type="text"
+          value={sentences[currentSentenceIdx]?.text || ""}
           onChange={handleTextChange}
         />
 
         <div
-          className='edit-controls-container'
+          className="edit-controls-container"
           style={{
-            pointerEvents: imageUrl ? 'auto' : 'none',
-            cursor: imageUrl ? 'auto' : 'not-allowed',
+            pointerEvents: imageUrl ? "auto" : "none",
+            cursor: imageUrl ? "auto" : "not-allowed",
           }}
         >
           <span
             onClick={imageUrl ? handleShowColorModal : null}
-            role='img'
-            aria-label='color-picker'
+            role="img"
+            aria-label="color-picker"
           >
-            <ColorPick className='color-pick-icon' />
+            <ColorPick className="color-pick-icon" />
           </span>
           <FontFamily
-            onSelectFontFamily={imageUrl ? handleFontFamilySelect : null}
+            onSelectFontFamily={
+              imageUrl
+                ? (selectedFont) =>
+                    handlePropertySelect("fontFamily", selectedFont)
+                : null
+            }
           />
           <button onClick={handleAddSentence}>+</button>
           <button onClick={handleSwitchSentence}>
@@ -312,15 +311,19 @@ export function CreateStory(props) {
         {showColorModal && (
           <div ref={colorModalRef}>
             <CanvasColorPicker
-              onColorSelect={handleColorSelect}
-              onWidthSelect={handleWidthSelect}
+              onColorSelect={(selectedColor) =>
+                handlePropertySelect("color", selectedColor)
+              }
+              onWidthSelect={(selectedWidth) =>
+                handlePropertySelect("width", selectedWidth)
+              }
               show={true}
               important={false}
             />
           </div>
         )}
 
-        <button className='add-story-button' onClick={addToStory}>
+        <button className="add-story-button" onClick={addToStory}>
           Add to Story
         </button>
       </div>
