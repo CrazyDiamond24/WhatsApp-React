4c20ff71a83a7fa3981277ca1c76ab7d012ac695
islam abo mokh <alo.jalal.55@gmail.com>
2023-09-07 18:42:15 +0300

    add handleMouseDown and handleMouseUp to the story modal

--- src/cmps/StoryModal.jsx
+++ src/cmps/StoryModal.jsx
@@ -3,26 +3,46 @@ import React, { useState, useEffect } from 'react'
 export function StoryModal({ user, closeStoryModal }) {
   const [currentStoryIndex, setCurrentStoryIndex] = useState(0)
   const [progress, setProgress] = useState(0)
+  const [progressInterval, setProgressInterval] = useState(null)
 
   useEffect(() => {
-    const progressInterval = setInterval(() => {
-      setProgress((prevProgress) => {
-        if (prevProgress >= 100) {
-          if (currentStoryIndex < user?.story?.length - 1) {
-            setCurrentStoryIndex((prevIndex) => prevIndex + 1)
-            return 0
-          }
-          clearInterval(progressInterval)
-          closeStoryModal()
-          return prevProgress
-        }
-        return prevProgress + 1
-      })
-    }, 70)
+    startProgress()
 
     return () => clearInterval(progressInterval)
+  }, [])
+
+  useEffect(() => {
+    setProgress(0)
+    startProgress()
   }, [currentStoryIndex])
 
+  function startProgress() {
+    setProgressInterval((prevInterval) => {
+      clearInterval(prevInterval)
+      return setInterval(() => {
+        setProgress((prevProgress) => {
+          if (prevProgress >= 100) {
+            if (currentStoryIndex < user?.story?.length - 1) {
+              setCurrentStoryIndex((prevIndex) => prevIndex + 1)
+              return 0
+            }
+            clearInterval(progressInterval)
+            closeStoryModal()
+            return prevProgress
+          }
+          return prevProgress + 1
+        })
+      }, 70)
+    })
+  }
+
+  function handleMouseDown() {
+    clearInterval(progressInterval)
+  }
+
+  function handleMouseUp() {
+    startProgress()
+  }
   function nextStory() {
     if (currentStoryIndex < user?.story?.length - 1) {
       setCurrentStoryIndex(currentStoryIndex + 1)
@@ -40,7 +60,11 @@ export function StoryModal({ user, closeStoryModal }) {
   }
 
   return (
-    <div className="story-modal">
+    <div
+      className="story-modal"
+      onMouseDown={handleMouseDown}
+      onMouseUp={handleMouseUp}
+    >
       <div className="progress-bar" style={{ width: `${progress}%` }}></div>
       <button onClick={prevStory} className="arrow-button left">
         ←
